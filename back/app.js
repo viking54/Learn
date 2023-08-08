77de9bb50983d9f9ff312e1516ab01ca27e6f4d9
viking54 <86645428+viking54@users.noreply.github.com>
2023-08-08 19:36:46 +0530

    Update app.js

--- back/app.js
+++ back/app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: "./config.env" });
 require("./db/conn");
 app.use(express.json());
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://gorgeous-belekoy-ea6081.netlify.app', credentials: true }));
 app.use("/uploads", express.static("uploads"));
 app.use("/api/task", taskRoutes);
 app.use("/api/sellertask", sellerTaskRoutes);
